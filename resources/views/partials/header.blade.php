31473ac24960895d53a32ffb3319e3a7126460f8
Kandarp Patel <kandarp.1310@gmail.com>
2024-01-31 01:10:58 -0500

    fix vite bootstrap import typo & change navbar icon

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,9 +3,12 @@
     <nav class="navbar navbar-expand-lg bg-light py-2">
         <div class="container">
             <a class="navbar-brand" href="{{ route('home') }}">{{ config('app.name') }}</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-nav"
+            <button class="navbar-toggler border-0 p-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-nav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line>
+                    <line x1="3" y1="18" x2="21" y2="18"></line>
+                </svg>
             </button>
             <div class="collapse navbar-collapse" id="navbar-nav">
                 <ul class="navbar-nav ms-auto">
