560f6af2327028b12e9f556229c7b25c141a06e7
Lakshit Gajera <lakshitgajera2001@gmail.com>
2024-02-16 22:15:05 -0500

    Add Search Reset Button

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -15,6 +15,12 @@
     <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line></svg>
   </button>
 
+  @if (request('search'))
+    <a href="{{ route('projects.index') }}" class="btn btn-primary ms-1 d-flex align-items-center py-2">
+      <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+    </a>
+  @endif
+
 </form>
   
 
