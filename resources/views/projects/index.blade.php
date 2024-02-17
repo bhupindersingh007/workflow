3590e3dcc755632c890443f235bd0b6e964524bf
Lakshit Gajera <lakshitgajera2001@gmail.com>
2024-02-16 23:09:30 -0500

    Update Project Page Buttons

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -59,15 +59,22 @@
         <td>{{ 0 }}</td>
         <td>{{ 0 }}</td>
         <td>
-          <button type="button" class="btn btn-sm btn-primary">View</button>
-          <a href="{{ route('projects.edit', ['project' => $project]) }}" class="btn btn-sm btn-success">Edit</a>
+          <button type="button" class="btn btn-sm text-primary">
+            <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+          </button>
+          
+          <a href="{{ route('projects.edit', ['project' => $project]) }}" class="btn btn-sm">
+            <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+          </a>
 
           {{-- Delete Project --}}
           <form action="{{ route('projects.destroy', ['project'=> $project]) }}" method="POST" class="d-inline-block"
             onsubmit="confirm('Are you sure?');">
             @csrf
             @method('DELETE')
-            <button type="submit" class="btn btn-sm btn-danger">Delete</button>
+            <button type="submit" class="btn btn-sm text-danger">
+              <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="css-i6dzq1"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+            </button>
           </form>
 
         </td>
