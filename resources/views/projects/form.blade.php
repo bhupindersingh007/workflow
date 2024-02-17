69967d4f86b3e1c7a0f43638d6ff14c2d083b461
Lakshit Gajera <lakshitgajera2001@gmail.com>
2024-02-16 21:59:39 -0500

    Update Project Form

--- resources/views/projects/form.blade.php
+++ resources/views/projects/form.blade.php
@@ -3,14 +3,14 @@
 
 <div class="mb-3">
   <label for="title" class="form-label">Title</label>
-  <input type="text" class="form-control" id="title" placeholder="Title" name="title" value="{{ $project->title ?? '' }}">
+  <input type="text" class="form-control" id="title" placeholder="Title" name="title" value="{{ $project->title ?? old('title') }}">
   @error('title')
   <small class="text-danger">{{ $message }}</small>
   @enderror
 </div>
 <div class="mb-3">
   <label for="description" class="form-label">Description</label>
-  <textarea class="form-control" id="description" rows="3" placeholder="Description" name="description">{{ $project->description ?? '' }}</textarea>
+  <textarea class="form-control" id="description" rows="3" placeholder="Description" name="description">{{ $project->description ?? old('description') }}</textarea>
   @error('description')
   <small class="text-danger">{{ $message }}</small>
   @enderror
