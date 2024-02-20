1e7aae5bc5dd97bd47b53e05efafb0033d202821
Kandarp Patel <kandarp.1310@gmail.com>
2024-02-19 22:53:11 -0500

    Add Assigned Task Frontend

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -4,10 +4,10 @@
 
 <header class="d-flex justify-content-between align-items-center mb-4">
   
-    <h5 class="mb-0">{{ $project->title }}</h5>
+    <h5 class="mb-0">Tasks</h5>
   
     {{-- Tasks Search --}}
-    <form class="d-flex align-items-center" action="{{ route('projects.tasks', ['project' => $project]) }}" method="GET">
+    <form class="d-flex align-items-center" method="GET" action="{{ route('projects.index') }}">
 
       
       <a class="btn btn-primary me-1 d-flex align-items-center py-2" data-bs-toggle="modal" data-bs-target="#filters-modal">
@@ -36,39 +36,27 @@
       </a>
       @endif
   
-      {{--Open New Project Task Modal --}}
-      <a href="#" class="btn btn-primary ms-1" data-bs-toggle="modal" data-bs-target="#task-modal">
-        &plus; Task
-      </a>
 
     </form>
 
-    
-    {{-- New Project Task Modal --}}
-    @include('projects.modal')
-
-    {{-- Project Task Filters Modal --}}
-    @include('projects.filters')
-
-    
-
-
+    @include('tasks.filters')
   
   </header>
 
 
-{{--Project Tasks --}}
+{{--Tasks --}}
   
 @if (true)
 <div class="table-responsive">
   <table class="table table-striped border">
-    <thead>
+    <thead class="small">
     <tr>
       <th>Title</th>
       <th>Status</th>
-      <th>Assigned To</th>
       <th>Deadline Date</th>
       <th>Priority</th>
+      <th>Project</th>
+      <th>Assigned By</th>
       <th>Actions</th>
     </tr>
     </thead>
@@ -77,27 +65,17 @@
       <tr>
         <td>Task {{ $i }}</td>
         <td><span class="text-danger">&#9679;</span> To Do</td>
-        <td>User {{ $i }}</td>
         <td>{{ now()->format('d M, Y') }}</td>
         <td><span class="text-danger">&#9679;</span> Low</td>
+        <td><a href="#" class="text-body">Project {{ $i }}</a></td>
+        <td>User {{ $i }}</td>
         <td>
           
-          <a href="#" class="btn btn-sm">
+          <a href="#" class="btn btn-sm" data-bs-toggle="modal" data-bs-target="#task-modal">
             <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
           </a>
-          
-          <a href="#" class="btn btn-sm">
-            <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-          </a>
 
-          <form action="#" method="POST" class="d-inline-block"
-            onsubmit="confirm('Are you sure?');">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-sm text-danger">
-              <svg viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
-            </button>
-          </form>
+          @include('tasks.show')
 
         </td>
       </tr>
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,16 @@
+{{-- Project Task Filters Modal --}}
+
+<div class="modal fade" id="task-modal" tabindex="-1" aria-labelledby="task-modal-label" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="filters-modal-label">Task 1</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                
+            </div>
+        </div>
+    </div>
+</div>
+
