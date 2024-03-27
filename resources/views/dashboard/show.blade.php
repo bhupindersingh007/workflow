8f6dd5629f7df1618b909f4600491fa71ccb2391
Bhupinder Singh <bs082045@gmail.com>
2024-03-27 14:50:02 -0400

    Add view more link dashboard widget

--- resources/views/dashboard/show.blade.php
+++ resources/views/dashboard/show.blade.php
@@ -123,9 +123,13 @@
 
     <div class="col-md-6 col-lg-8">
         <div class="card">
-            <div class="card-header bg-white fw-bold d-flex align-items-center">
-                <svg class="me-1" viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
-                Latest Tasks
+            <div class="card-header bg-white d-flex align-items-center justify-content-between">
+                <span class="d-flex align-items-center">
+                    <svg class="me-1" viewBox="0 0 24 24" width="18" height="18" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg>
+                    <span class="fw-bold">Latest Tasks</span>
+                </span>
+                
+                <a href="{{ route('tasks.index') }}" class="text-muted">View More</a>
             </div>
             <div class="card-body px-0">
                 <table class="table table-sm">
